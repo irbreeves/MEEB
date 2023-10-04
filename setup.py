1c55d16e920b32b390369f7c74c10c10a339270f
ireeves_USGS <ireeves@usgs.gov>
2023-10-04 14:54:51 -0400

    update setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Ian Reeves",
-    author_email="ireeves@whoi.edu",
+    author_email="ireeves@usgs.gov",
     url="https://github.com/irbreeves/meeb",
     classifiers=[
         "Intended Audience :: Science/Research",
@@ -16,11 +16,12 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Scientific/Engineering :: Physics",
     ],
     keywords=["barrier", "dune", "overwash", "coastal", "aeolian", "vegetation", "earth science"],
     install_requires=open("requirements.txt", "r").read().splitlines(),
     include_package_data=True,
     packages=find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.6, <3.12.*",
 )
